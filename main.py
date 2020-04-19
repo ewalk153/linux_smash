ad95bed82575be394b1fd6fd342ba142505f7c18
Eric Walker <eric.ewalker@gmail.com>
2020-04-19 15:03:53 -0400

    Basic setup of the app, with some sound

--- main.py
+++ main.py
@@ -1,37 +1,76 @@
-import sys
+import sys, random
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
 from PyQt5.QtGui import QIcon
+from PyQt5.QtMultimedia import QSound
+from PyQt5.QtCore import Qt
 
 class App(QMainWindow):
+    WIDTH = 440
+    HEIGHT = 280
+    MAX_LETTERS = 5
+    labels = []
 
-    def __init__(self):
+    def keyPressEvent(self, event):
+        key = event.key()
+        if key == Qt.Key_Return:
+            print('return key pressed')
+        else:
+            if self.showable(key):
+                char = chr(key)
+                # print('key pressed: %s' % char)
+                self.showChar(char)
+
+    def showable(self, key):
+        return (key >= ord('A') and key <= ord('Z')) or (key >= ord('A') and key <= ord('Z'))
+
+    def showChar(self, char):
+        label = QLabel(char, self)
+        label.move(
+            random.randint(0, self.width),
+            random.randint(0, self.height))
+        label.show()
+
+        self.labels.append(label)
+        self.sounds.play(char)
+        self.show()
+        
+        if len(self.labels) > self.MAX_LETTERS:
+            l = self.labels.pop(0)
+            l.deleteLater()
+
+    def __init__(self, soundbank):
         super().__init__()
         self.title = 'PyQt absolute positioning - pythonspot.com'
         self.left = 10
         self.top = 10
-        self.width = 440
-        self.height = 280
+        self.width = self.WIDTH
+        self.height = self.HEIGHT
+        self.showFullScreen()
         self.initUI()
+
+        self.sounds = soundbank
+
+
+        self.setStyleSheet("QLabel {font: 30pt Helvetica}")
     
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        
-        label = QLabel('Python', self)
-        label.move(50,50)
-        
-        label2 = QLabel('PyQt5', self)
-        label2.move(100,100)
-        
-        label3 = QLabel('Examples', self)
-        label3.move(150,150)
-        
-        label4 = QLabel('pytonspot.com', self)
-        label4.move(200,200)
-        
         self.show()
+
+class SoundBank():
+    # calling this initialization here seems to bork
+    # things with QEventLoop: Cannot be used without QApplication
+    NOTES = {
+        'A': QSound("sounds/A.wav")
+    }
+
+    def play(self, letter):
+        if letter in self.NOTES:
+            self.NOTES[letter].play()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    sounds = SoundBank()
+    ex = App(sounds)
     sys.exit(app.exec_())
